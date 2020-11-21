e0e6cfa4f1ba5492a6d1cad1352d7774e7c9e556
Akshay Kakatkar <cryllicbuster273@pixelexperience.org>
2020-11-21 18:18:42 +0100

    gauguin: power: Rebase on LA.UM.9.1.r1.07700-SMxxx0.0
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>
    
    Conflicts:
    	device.mk
    	power/Android.mk
    	power/android.hardware.power-service.rc
    	power/android.hardware.power@1.2-service.rc
    	sepolicy/private/file_contexts

--- power/Android.mk
+++ power/Android.mk
@@ -4,18 +4,18 @@ LOCAL_PATH := $(call my-dir)
 # HAL module implemenation stored in
 # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.power@1.2-service.gauguin
+LOCAL_MODULE := android.hardware.power-service.gauguin
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE_STEM := android.hardware.power@1.2-service
+LOCAL_MODULE_STEM := android.hardware.power-service
 
-LOCAL_REQUIRED_MODULES := android.hardware.power@1.2-service.rc
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libhidlbase libhidltransport libutils android.hardware.power@1.2
+LOCAL_REQUIRED_MODULES := android.hardware.power-service.rc
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libbinder_ndk libutils android.hardware.power-ndk_platform
 LOCAL_HEADER_LIBRARIES += libutils_headers
 LOCAL_HEADER_LIBRARIES += libhardware_headers
-LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c service.cpp Power.cpp
+LOCAL_SRC_FILES := power-common.c main.cpp metadata-parser.c utils.c list.c hint-data.c powerhintparser.c Power.cpp
 LOCAL_C_INCLUDES := external/libxml2/include \
                     external/icu/icu4c/source/common
 
@@ -27,3 +27,12 @@ ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
     LOCAL_CFLAGS += -DINTERACTION_BOOST
 endif
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.power-service.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
+LOCAL_MODULE_STEM := android.hardware.power-service.rc
+LOCAL_SRC_FILES := android.hardware.power-service.rc
+include $(BUILD_PREBUILT)
+
